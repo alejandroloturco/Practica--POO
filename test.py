321cd0173699dbae5e26726b6c9a80d2f280b486
alejandroloturco <alejandro.loturco@udea.edu.co>
2024-03-06 01:28:04 -0500

    aqui

--- test.py
+++ test.py
@@ -1,4 +1,25 @@
 from Clase4_V3 import *
 
-#pac = Paciente()
-print("hola")
+while True:
+    x= int(input(":"))
+    if x  == 1:
+        pac = Paciente()
+        pac.asignarCedula(12345)
+        pac.asignarGenero("M")
+        pac.asignarNombre("Alejo loturco")
+        pac.asignarServicio("Cardiologo")
+
+        sis = Sistema()
+        sis.ingresarPaciente(pac)
+    elif x==2:
+        pacs = sis.verDatosPaciente("alejo") 
+        if pacs != None:
+                for p in pacs:
+                    print("Nombre: " + p.verNombre()) 
+                    print("Cedula: " + str(p.verCedula())) 
+                    print("Genero: " + p.verGenero()) 
+                    print("Servicio: " + p.verServicio())
+       
+    elif x==3:
+        break
+
